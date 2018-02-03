8da28b25a9e01ccaf48086616eb9f92db53dad34
Romain Couperier <romain.couperier@mail.mcgill.ca>
2018-02-03 15:27:34 -0500

    Test Success

---
+++ Test.ino
@@ -0,0 +1,27 @@
+#include "cellular_hal.h"
+
+// Set the TELUS APN
+STARTUP(cellular_credentials_set("isp.telus.com", "", "", NULL));
+
+int led2 = D7;
+
+void setup() {
+  // Set the keep-alive value for TELUS SIM card
+  Particle.keepAlive(30);
+
+  pinMode(led2, OUTPUT);
+
+}
+
+void loop () {
+
+   digitalWrite(led2, HIGH);
+
+   delay(1000);
+
+   digitalWrite(led2, LOW);
+
+   delay(1000);
+
+Particle.publish("test");
+}
